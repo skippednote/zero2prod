88db0fb9bf6027f5dbc96baeea8c6f38936cb512
Bassam Ismail <skippednote@gmail.com>
2021-12-22 16:10:59 +0530

    feat: health_check

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,8 @@
-fn main() {
-    println!("Hello, world!");
+use std::net::TcpListener;
+use zero2prod::run;
+
+#[actix_web::main]
+async fn main() -> std::io::Result<()> {
+    let listener = TcpListener::bind("127.0.0.1:8080").expect("Failed to bind port");
+    run(listener)?.await
 }
