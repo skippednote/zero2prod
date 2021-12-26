8593482f6525d48eb30cfa42e8c2e9f62b59242f
Bassam Ismail <skippednote@gmail.com>
2021-12-27 00:04:35 +0530

    chore: fix test and db

--- tests/health_check.rs
+++ tests/health_check.rs
@@ -67,7 +67,7 @@ pub async fn configure_database(config: &DatabaseSettings) -> PgPool {
         .execute(format!(r#"CREATE DATABASE "{}";"#, config.database_name).as_str())
         .await
         .expect("Failed to create database.");
-    let connection_pool = PgPool::connect_with(config.without_db())
+    let connection_pool = PgPool::connect_with(config.with_db())
         .await
         .expect("Failed to connect to Postgres.");
     sqlx::migrate!("./migrations")
